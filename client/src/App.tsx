53f932cb17cd2128366071f6ced66035daea63db
as8432081 <as8432081@gmail.com>
2025-08-31 17:34:22 +0530

    Update App.tsx
    update for url

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router base="/vks">
+        <Router base="/">
           <Switch>
             <Route path="/" component={Home} />
             <Route component={NotFound} />
